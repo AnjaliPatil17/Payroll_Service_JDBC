903b12c86b4c8a00cf432c42f53bb9ec89d78cb6
Anjali <anjaliap1227@gmail.com>
2024-06-28 17:49:32 +0530

    Welcome to JDBC Connection

---
+++ src/main/java/Payroll_Service_JDBC.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Payroll_Service_JDBC{
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String user = "root";
+        String password = "Ap1227@#";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+            if (con != null) {
+                System.out.println("Database connected successfully!");
+                System.out.println("Connection Info: " + con);
+                java.sql.Driver driver = DriverManager.getDriver(url);
+                System.out.println("JDBC Driver: " + driver.getClass().getName());
+                java.util.Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
+                while (driverList.hasMoreElements()) {
+                    java.sql.Driver d = driverList.nextElement();
+                    System.out.println("Registered JDBC Driver: " + d.getClass().getName());
+                }
+
+                con.close();
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
