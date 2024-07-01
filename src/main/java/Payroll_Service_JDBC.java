5de2e407cb92c40daf7f3d9e528266ab60734b8c
Anjali <anjaliap1227@gmail.com>
2024-07-01 15:11:54 +0530

    Ability to find sum, average, min, max and number of male and female employees

--- src/main/java/Payroll_Service_JDBC.java
+++ src/main/java/Payroll_Service_JDBC.java
@@ -1,12 +1,11 @@
-
 import java.sql.*;
 
-public class Payroll_Service_JDBC{
+public class Payroll_Service_JDBC {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/payroll_service";
         String user = "root";
         String password = "****";
-        String selectQuery = "SELECT * FROM employee_payroll WHERE start_date BETWEEN CAST('2024-01-03' AS DATE) AND DATE (NOW()) ";
+        String selectQuery = "SELECT * FROM employee_payroll WHERE start_date BETWEEN CAST('2024-01-03' AS DATE) AND DATE (NOW())";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,14 +13,19 @@ public class Payroll_Service_JDBC{
             if (con != null) {
                 System.out.println("Database connected successfully!");
                 System.out.println("Connection Info: " + con);
+
                 java.sql.Driver driver = DriverManager.getDriver(url);
                 System.out.println("JDBC Driver: " + driver.getClass().getName());
+
                 java.util.Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
                 while (driverList.hasMoreElements()) {
                     java.sql.Driver d = driverList.nextElement();
                     System.out.println("Registered JDBC Driver: " + d.getClass().getName());
                 }
+                System.out.println();
+
                 Statement stmt = con.createStatement();
+
 
                 ResultSet rs = stmt.executeQuery(selectQuery);
                 while (rs.next()) {
@@ -39,8 +43,49 @@ public class Payroll_Service_JDBC{
                     int Income_tax = rs.getInt("Income_tax");
                     int Net_pay = rs.getInt("Net_pay");
                     System.out.println(emp_id + " " + name + " " + gender + " " + salary + " " + start_date + " " + ph_number + " " + address + " " + department + " " + Basic_pay + " " + Deductions + " " + Taxable_pay + " " + Income_tax + " " + Net_pay);
-                    System.out.println();
                 }
+                System.out.println();
+
+                System.out.println("------ Sum of salary ------");
+                ResultSet rs1 = stmt.executeQuery("SELECT gender, SUM(salary) AS total_salary FROM employee_payroll WHERE gender = 'M' OR gender = 'F' GROUP BY gender");
+                while (rs1.next()) {
+                    System.out.println("Gender: " + rs1.getString("gender"));
+                    System.out.println("Total Salary: " + rs1.getDouble("total_salary"));
+                }
+                System.out.println();
+
+
+                System.out.println("------ Avg of salary ------");
+                ResultSet rs2 = stmt.executeQuery("SELECT gender, AVG(salary) AS avg_salary FROM employee_payroll WHERE gender = 'M' OR gender = 'F' GROUP BY gender");
+                while (rs2.next()) {
+                    System.out.println("Gender: " + rs2.getString("gender"));
+                    System.out.println("Avg Salary: " + rs2.getDouble("avg_salary"));
+                }
+                System.out.println();
+
+                System.out.println("------ Min of salary ------");
+                ResultSet rs3 = stmt.executeQuery("SELECT gender, MIN(salary) AS min_salary FROM employee_payroll WHERE gender = 'M' OR gender = 'F' GROUP BY gender");
+                while (rs3.next()) {
+                    System.out.println("Gender: " + rs3.getString("gender"));
+                    System.out.println("Min Salary: " + rs3.getDouble("min_salary"));
+                }
+                System.out.println();
+
+                System.out.println("------ Max of salary ------");
+                ResultSet rs4 = stmt.executeQuery("SELECT gender, MAX(salary) AS max_salary FROM employee_payroll WHERE gender = 'M' OR gender = 'F' GROUP BY gender");
+                while (rs4.next()) {
+                    System.out.println("Gender: " + rs4.getString("gender"));
+                    System.out.println("Max Salary: " + rs4.getDouble("max_salary"));
+                }
+                System.out.println();
+
+                System.out.println("------ Count of employees ------");
+                ResultSet rs5 = stmt.executeQuery("SELECT gender, COUNT(*) AS employee_count FROM employee_payroll WHERE gender = 'M' OR gender = 'F' GROUP BY gender");
+                while (rs5.next()) {
+                    System.out.println("Gender: " + rs5.getString("gender"));
+                    System.out.println("Employee Count: " + rs5.getInt("employee_count"));
+                }
+                System.out.println();
 
                 con.close();
             } else {
@@ -50,6 +95,5 @@ public class Payroll_Service_JDBC{
             System.out.println("Database connection failed!");
             e.printStackTrace();
         }
-
     }
 }
